8f810f09fa744bb0dd5c2e71696edecb47f253a0
epicgamer984 <75700755+epicgamer984@users.noreply.github.com>
2024-03-14 21:25:42 -0400

    Add files via upload
    gtag locomotion not mine but everything else i coded myself

---
+++ enemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    private AudioSource enemyAudio;
+    public AudioClip deathSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+            
+
+                
+            if (transform.position.y <-20){
+            Destroy(gameObject);
+            
+
+                }
+    }
+}
